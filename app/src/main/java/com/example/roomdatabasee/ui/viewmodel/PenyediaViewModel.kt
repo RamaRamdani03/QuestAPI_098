9dc1782b45c761f82f7f1b80bfe3f0b00c988cd6
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 11:08:43 +0700

    Menambahkan Factory untuk pembuatan ViewModel dengan viewModelFactory

--- app/src/main/java/com/example/roomdatabasee/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/viewmodel/PenyediaViewModel.kt
@@ -2,10 +2,13 @@ package com.example.roomdatabasee.ui.viewmodel
 
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabasee.MahasiswaApplications
 
 object PenyediaViewModel {
+    val Factory = viewModelFactory {
 
+    }
 }
 
 fun CreationExtras.aplikasiKontak(): MahasiswaApplications =
