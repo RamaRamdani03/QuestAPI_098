8892cf13b1cc7763d0939dccb7e59c101c64f8bc
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 07:24:28 +0700

    Implementasi MahasiswaContainer dengan konfigurasi Retrofit dan dependency mahasiswaRepository

--- app/src/main/java/com/example/roomdatabasee/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/roomdatabasee/dependenciesinjection/MahasiswaContainer.kt
@@ -1,7 +1,30 @@
 package com.example.roomdatabasee.dependenciesinjection
 
+import com.example.roomdatabasee.repository.NetworkMahasiswaRepository
 import com.example.roomdatabasee.repository.mahasiswaRepository
+import com.example.roomdatabasee.service_api.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val kontakRepository: mahasiswaRepository
+}
+
+class MahasiswaContainer: AppContainer{
+
+    private val baseUrl = "http://10.0.2.2:80/umyTI/" //localhost diganti ip kalo run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: mahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
 }
