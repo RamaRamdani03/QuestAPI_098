f19012c106a22fb36b62a0b66588986058baa158
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 12:00:28 +0700

    Logika di UpdateViewModel untuk mengambil data mahasiswa, memperbarui status, dan menyimpan pembaruan

--- app/src/main/java/com/example/roomdatabasee/ui/viewmodel/UpadteViewModel.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/viewmodel/UpadteViewModel.kt
@@ -1,12 +1,42 @@
 package com.example.roomdatabasee.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdatabasee.repository.mahasiswaRepository
+import com.example.roomdatabasee.ui.view.DestinasiUpdate
+import kotlinx.coroutines.launch
 
 class UpdateViewModel(
     savedStateHandle: SavedStateHandle,
     private val mahasiswaRepository: mahasiswaRepository
 ): ViewModel(){
+    var updateUiState by mutableStateOf(InsertUiState())
+        private set
 
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = mahasiswaRepository.getMahasiswaById(_nim)
+                .toUiStateMhs()
+        }
+    }
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        updateUiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun updateMhs(){
+        viewModelScope.launch {
+            try {
+                mahasiswaRepository.updateMahasiswa(_nim, updateUiState.insertUiEvent.toMhs())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
