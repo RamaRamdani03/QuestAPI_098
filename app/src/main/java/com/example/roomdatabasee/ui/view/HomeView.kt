c3098307cf45cb147d5152022ffef4bd5153d47a
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 09:00:52 +0700

    Menambahkan object DestinasiHome untuk mendefinisikan route dan title untuk halaman Home Mhs

---
+++ app/src/main/java/com/example/roomdatabasee/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabasee.ui.view
+
+import com.example.roomdatabasee.ui.navigasi.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
