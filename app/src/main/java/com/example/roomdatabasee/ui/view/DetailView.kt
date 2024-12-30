86048e005c0d565720765a8e1b4238d4e04a0794
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 11:21:39 +0700

    Composable ComponentDetailMhs untuk menampilkan detail informasi mahasiswa

--- app/src/main/java/com/example/roomdatabasee/ui/view/DetailView.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/view/DetailView.kt
@@ -1,5 +1,13 @@
 package com.example.roomdatabasee.ui.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.roomdatabasee.ui.navigasi.DestinasiNavigasi
 
 object DestinasiDetail: DestinasiNavigasi{
@@ -7,4 +15,27 @@ object DestinasiDetail: DestinasiNavigasi{
     override val titleRes = "Detail Mhs"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
