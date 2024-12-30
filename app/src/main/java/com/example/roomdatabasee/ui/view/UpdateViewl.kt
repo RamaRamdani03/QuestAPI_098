d3b64b41f31e56353bd86526154678d898ce5fa9
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 11:57:52 +0700

    Menambahkan Composable UpdateScreen untuk menangani layar update mahasiswa dengan navigasi dan aksi kembali

--- app/src/main/java/com/example/roomdatabasee/ui/view/UpdateViewl.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/view/UpdateViewl.kt
@@ -1,10 +1,27 @@
 package com.example.roomdatabasee.ui.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.roomdatabasee.ui.navigasi.DestinasiNavigasi
+import com.example.roomdatabasee.ui.viewmodel.PenyediaViewModel
+import com.example.roomdatabasee.ui.viewmodel.UpdateViewModel
 
 object DestinasiUpdate: DestinasiNavigasi {
     override val route = "update"
     override val titleRes = "Update Mhs"
     const val NIM = "nim"
     val routesWithArg = "$route/{$NIM}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateScreen(
+    onBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    onNavigate:()-> Unit,
+    viewModel: UpdateViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
