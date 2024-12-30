25c39b58535435195bcc23c968805dea8e40b1cf
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 08:55:10 +0700

    Menambahkan object DestinasiEntry untuk mendefinisikan route dan title untuk halaman Entry Mhs

---
+++ app/src/main/java/com/example/roomdatabasee/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabasee.ui.view
+
+import com.example.roomdatabasee.ui.navigasi.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
