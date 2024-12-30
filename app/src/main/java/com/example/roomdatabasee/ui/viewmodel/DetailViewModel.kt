200a529f48a19323c7e7fc8f5b43ab7cdbbbbbef
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 11:30:00 +0700

    Menambahkan ViewModel DetailViewModel untuk menangani logika dan state terkait detail mahasiswa

--- app/src/main/java/com/example/roomdatabasee/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/viewmodel/DetailViewModel.kt
@@ -1,9 +1,19 @@
 package com.example.roomdatabasee.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.roomdatabasee.model.Mahasiswa
+import com.example.roomdatabasee.repository.mahasiswaRepository
 
 sealed class DetailUiState {
     data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
     object Error : DetailUiState()
     object Loading : DetailUiState()
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mhs: mahasiswaRepository
+) : ViewModel() {
+
 }
