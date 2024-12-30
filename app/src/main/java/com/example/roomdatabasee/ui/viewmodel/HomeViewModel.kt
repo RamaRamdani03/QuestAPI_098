5b968e70fe574500f69c5aa5383032b9ebde9ea6
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 08:50:26 +0700

    Menambahkan fungsi deleteMhs() di HomeViewModel untuk menghapus data mahasiswa

--- app/src/main/java/com/example/roomdatabasee/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/viewmodel/HomeViewModel.kt
@@ -5,9 +5,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import coil.network.HttpException
 import com.example.roomdatabasee.model.Mahasiswa
 import com.example.roomdatabasee.repository.mahasiswaRepository
 import kotlinx.coroutines.launch
+import okio.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
@@ -31,6 +33,18 @@ class HomeViewModel (private val mhs: mahasiswaRepository): ViewModel(){
             } catch (e: Exception) {
                 HomeUiState.Error
             } catch (e: Exception) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
                 HomeUiState.Error
             }
         }
