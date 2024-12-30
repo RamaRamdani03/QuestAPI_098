3c821701427788cbb270a641c4ac7e4d2b72e0c9
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 07:38:40 +0700

    Inisialisasi AppContainer pada onCreate di MahasiswaApplications

--- app/src/main/java/com/example/roomdatabasee/MahasiswaApplications.kt
+++ app/src/main/java/com/example/roomdatabasee/MahasiswaApplications.kt
@@ -1,7 +1,13 @@
 package com.example.roomdatabasee
 
 import android.app.Application
+import com.example.roomdatabasee.dependenciesinjection.AppContainer
+import com.example.roomdatabasee.dependenciesinjection.MahasiswaContainer
 
 class MahasiswaApplications : Application(){
-
+    lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
 }
