22874b18022665058b43bf6f2649ab6a9a1f8b3d
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 11:14:03 +0700

    Menambahkan Composable PengelolaHalaman untuk mengatur navigasi antar halaman menggunakan NavHostController

---
+++ app/src/main/java/com/example/roomdatabasee/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.roomdatabasee.ui.navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.roomdatabasee.ui.view.DestinasiDetail
+import com.example.roomdatabasee.ui.view.DestinasiEntry
+import com.example.roomdatabasee.ui.view.DestinasiHome
+import com.example.roomdatabasee.ui.view.EntryMhsScreen
+import com.example.roomdatabasee.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = {
+                    navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
