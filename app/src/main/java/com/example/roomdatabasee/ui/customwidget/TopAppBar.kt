234fbdb7c02071cbafee6aa28e3386072f14fbbb
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 08:53:41 +0700

    Menambahkan implementasi CenterAlignedTopAppBar dengan ikon navigasi dan aksi Refresh

--- app/src/main/java/com/example/roomdatabasee/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/customwidget/TopAppBar.kt
@@ -1,6 +1,14 @@
 package com.example.roomdatabasee.ui.customwidget
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,5 +22,20 @@ fun CostumeTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {},
 ) {
+    CenterAlignedTopAppBar(
+        title = {Text(title) },
+        actions = {
+            Icon(imageVector = Icons.Default.Refresh, contentDescription = "", modifier = Modifier.clickable {
 
+            })
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior, navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = null)
+                }
+            }
+        }
+    )
 }
