7ee7c918587d112210f21df1ed151454c9dd381e
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 08:48:55 +0700

    Menambahkan HomeViewModel untuk mengelola status UI dan mengambil data mahasiswa

--- app/src/main/java/com/example/roomdatabasee/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdatabasee/ui/viewmodel/HomeViewModel.kt
@@ -1,9 +1,34 @@
 package com.example.roomdatabasee.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.roomdatabasee.model.Mahasiswa
+import com.example.roomdatabasee.repository.mahasiswaRepository
+import kotlinx.coroutines.launch
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>): HomeUiState()
     object Error : HomeUiState()
     object Loading : HomeUiState()
+}
+
+class HomeViewModel (private val mhs: mahasiswaRepository): ViewModel() {
+    var mhsUiState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUiState = HomeUiState.Loading
+            mhsUiState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: Exception) {
+                HomeUiState.Error
+            } catch (e: Exception) {
+                HomeUiState.Error
+            }
+        }
+    }
 }
