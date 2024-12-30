db1f1ab4d743f584ce732578fba5d003082d5974
RamaRamdani03 <ramaramdani449@gamil.com>
2024-12-30 11:59:45 +0700

    Menambahkan ViewModel UpdateViewModel untuk menangani logika dan state terkait pembaruan data mahasiswa

---
+++ app/src/main/java/com/example/roomdatabasee/ui/viewmodel/UpadteViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.roomdatabasee.ui.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.roomdatabasee.repository.mahasiswaRepository
+
+class UpdateViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mahasiswaRepository: mahasiswaRepository
+): ViewModel(){
+
+}
